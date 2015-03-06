ede62fe209d4976ad4c2d4e87e915f82a4648ea8
Pratima Gauns <pratima.gauns@intouchsol.com>
2015-03-06 18:18:18 +0530

    Initial Commit

---
+++ Checkers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Checkers
+//
+//  Created by Pratima Gauns on 06/03/15.
+//  Copyright (c) 2015 VP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Checkers/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Checkers
+//
+//  Created by Pratima Gauns on 06/03/15.
+//  Copyright (c) 2015 VP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
