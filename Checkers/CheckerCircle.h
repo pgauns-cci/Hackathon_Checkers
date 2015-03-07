cd9d18a102c7f3d3c5c1d9ad5ef98bcdefb888a9
Vinnie Da Silva <vinnie.dasilva@creativecapsule.com>
2015-03-07 19:40:28 +0530

    Added CheckerCircle

---
+++ Checkers/CheckerCircle.h
@@ -0,0 +1,16 @@
+//
+//  CheckerCircle.h
+//  Checkers
+//
+//  Created by Vinnie Da Silva on 07/03/15.
+//  Copyright (c) 2015 VP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckerCircle : NSObject
+
+@property (nonatomic, readwrite) CGFloat radius;
+@property (nonatomic, readwrite) CGPoint center;
+
+@end
---
+++ Checkers/CheckerCircle.m
@@ -0,0 +1,13 @@
+//
+//  CheckerCircle.m
+//  Checkers
+//
+//  Created by Vinnie Da Silva on 07/03/15.
+//  Copyright (c) 2015 VP. All rights reserved.
+//
+
+#import "CheckerCircle.h"
+
+@implementation CheckerCircle
+
+@end
